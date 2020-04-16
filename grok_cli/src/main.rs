8e86b28bddf61f9639203870f317dac9f0f8dbe4
James Birchfield <jbirchfield@shopstyle.com>
2020-04-16 17:10:18 -0500

    add the ability to filter to a specific log level

--- grok_cli/src/main.rs
+++ grok_cli/src/main.rs
@@ -1,40 +1,53 @@
+extern crate clap;
 extern crate grok_lib;
-
-use std::io::{self, BufRead};
+use chrono::{TimeZone, Utc};
+use clap::{App, Arg};
 use grok_lib::log_json::module_json::*;
-use chrono::{DateTime, TimeZone, Utc};
-
+use std::io::{self, BufRead};
 
 fn main() {
+    let matches = App::new("grok")
+        .version("0.1.0")
+        .author("James Birchfield <jbirchfield@shopstyle.com>")
+        .about("Reshapes JSON logging")
+        .arg(
+            Arg::with_name("level")
+                .short("l")
+                .possible_values(&["ALL", "TRACE", "DEBUG", "WARN", "INFO", "ERROR"])
+                .default_value("ALL")
+                .long("level")
+                .takes_value(true)
+                .help("filter to a certain log level"),
+        )
+        .get_matches();
+    let level = matches.value_of("level").unwrap_or("ALL");
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from standard in");
         let jm: JSONMessage = serde_json::from_str(&line).unwrap();
-        let dt = Utc.timestamp((jm.timeMillis/ 1000) as i64, 0);
-        println!(
-            "{} [{}] {} {} - {}",
-            dt.to_rfc3339(),
-            jm.thread,
-            jm.level,
-            jm.loggerName,
-            jm.message
-        );
-        match jm.thrown {
-            Some(t) => {
-                println!("{}", t.name);
-                for trace in t.extendedStackTrace {
-                    let file = show_file(trace.file);
-                    println!(
-                        "\t at {}.{} ({}:{}) [{}]",
-                        trace.class, trace.method, file, trace.line, trace.location
-                    );
+        if level == "ALL" || level == jm.level {
+            let dt = Utc.timestamp((jm.timeMillis / 1000) as i64, 0);
+            println!(
+                "{} [{}] {} {} - {}",
+                dt.to_rfc3339(),
+                jm.thread,
+                jm.level,
+                jm.loggerName,
+                jm.message
+            );
+            match jm.thrown {
+                Some(t) => {
+                    println!("{}", t.name);
+                    for trace in t.extendedStackTrace {
+                        let file = trace.file.unwrap_or("Unknown".to_string());
+                        println!(
+                            "\t at {}.{} ({}:{}) [{}]",
+                            trace.class, trace.method, file, trace.line, trace.location
+                        );
+                    }
                 }
+                None => {}
             }
-            None => {}
         }
-    }
-
-    fn show_file(name: Option<String>) -> String {
-        return name.unwrap_or("Unknown".to_string());
     }
 }
