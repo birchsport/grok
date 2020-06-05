131ebe0f167402fd2b2500749b79b18844633996
James Birchfield <jbirchfield@shopstyle.com>
2020-06-05 15:25:51 -0500

    beefed up error handling

--- grok_cli/src/main.rs
+++ grok_cli/src/main.rs
@@ -34,61 +34,67 @@ fn main() {
     let level = matches.value_of("level").unwrap_or("ALL");
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        let line = line.expect("Could not read line from standard in");
-        let mut words: Vec<&str> = line.split_whitespace().collect();
-        &words.remove(0);
-        let instance = &words.remove(0);
-        let json = &words.join(" ");
-        let j = serde_json::from_str(&json);
-        match j {
+        match line {
             Ok(l) => {
-                let jm: JSONMessage = l;
-                if level == "ALL" || level == jm.level {
-                    let dt = Utc.timestamp((jm.timeMillis / 1000) as i64, 0);
-                    println!(
-                        "{}{} -- {} [{}] {}{} {}{} - {}{}{}",
-                        color_str(!nocolor, &color::Reset),
-                        instance,
-                        dt.to_rfc3339(),
-                        jm.thread,
-                        color_str(!nocolor, &color::Magenta),
-                        jm.level,
-                        color_str(!nocolor, &color::Reset),
-                        jm.loggerName,
-                        if jm.level == "ERROR" {
-                            color_str(!nocolor, &color::Red)
-                        } else if jm.level == "WARN" {
-                            color_str(!nocolor, &color::Yellow)
-                        } else {
-                            color_str(!nocolor, &color::Cyan)
-                        },
-                        jm.message,
-                        color_str(!nocolor, &color::Reset)
-                    );
-                    match jm.thrown {
-                        Some(t) => {
-                            println!("{}", t.name);
-                            for trace in t.extendedStackTrace {
-                                println!(
-                                    "\t at {}{}.{} ({}:{}) [{}]{}",
-                                    color_str(!nocolor, &color::Red),
-                                    trace.class,
-                                    trace.method,
-                                    trace.file.unwrap_or("Unknown".to_string()),
-                                    trace.line,
-                                    trace.location,
-                                    color_str(!nocolor, &color::Reset)
-                                );
+                let mut words: Vec<&str> = l.split_whitespace().collect();
+                &words.remove(0);
+                let instance = &words.remove(0);
+                let json = &words.join(" ");
+                let j = serde_json::from_str(&json);
+                match j {
+                    Ok(l) => {
+                        let jm: JSONMessage = l;
+                        if level == "ALL" || level == jm.level {
+                            let dt = Utc.timestamp((jm.timeMillis / 1000) as i64, 0);
+                            println!(
+                                "{}{} -- {} [{}] {}{} {}{} - {}{}{}",
+                                color_str(!nocolor, &color::Reset),
+                                instance,
+                                dt.to_rfc3339(),
+                                jm.thread,
+                                color_str(!nocolor, &color::Magenta),
+                                jm.level,
+                                color_str(!nocolor, &color::Reset),
+                                jm.loggerName,
+                                if jm.level == "ERROR" {
+                                    color_str(!nocolor, &color::Red)
+                                } else if jm.level == "WARN" {
+                                    color_str(!nocolor, &color::Yellow)
+                                } else {
+                                    color_str(!nocolor, &color::Cyan)
+                                },
+                                jm.message,
+                                color_str(!nocolor, &color::Reset)
+                            );
+                            match jm.thrown {
+                                Some(t) => {
+                                    println!("{}", t.name);
+                                    for trace in t.extendedStackTrace {
+                                        println!(
+                                            "\t at {}{}.{} ({}:{}) [{}]{}",
+                                            color_str(!nocolor, &color::Red),
+                                            trace.class,
+                                            trace.method,
+                                            trace.file.unwrap_or("Unknown".to_string()),
+                                            trace.line,
+                                            trace.location,
+                                            color_str(!nocolor, &color::Reset)
+                                        );
+                                    }
+                                }
+                                None => {
+                                    //swallow
+                                }
                             }
                         }
-                        None => {
-                            //swallow
-                        }
+                    }
+                    Err(e) => {
+                        println!("Unable to parse json {} :: input - {}", e.to_string(), l);
                     }
                 }
             }
             Err(e) => {
-                println!("Unable to parse line {} :: input - {}", e.to_string(), json);
+                println!("Unable to parse line {}", e.to_string());
             }
         }
     }
